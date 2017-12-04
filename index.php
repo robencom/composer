146c0631d238372fd40323c1f436a6cf45044f1c
robencom <robencom@hotmail.com>
2017-12-04 19:06:46 +0400

    edited index file

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 
 require 'vendor/autoload.php';
 
+
 use Rub\Class1;
 use Rub\Class2;
 
 $class1 = new Class1();
 $class2 = new Class2();
 
-//var_dump('INDEX');
+//var_dump('INDEX');jjj
 
 $class1->sth();
 $class2->sth();
